d812d9e20c323b5536dc2ae04ddeb0e34bd2e032
Eduardo Melo <meloedu105@gmail.com>
2024-11-14 20:07:51 -0300

    Atividade Flags pronta

---
+++ 05-Atividade/01-flags/script.js
@@ -0,0 +1,25 @@
+
+
+fetch('https://restcountries.com/v3.1/all?fields=name,flags,capital')
+.then(response => response.json())
+.then(data => exibirDados(data))
+.catch( erro => console.error('Estamos com problema', erro))
+
+
+function exibirDados(paises){
+    const box = document.querySelector("#box-pais")
+    box.innerHTML = '';
+    
+    paises.forEach(pais => {
+        box.innerHTML += 
+        `
+            <div class = "pais-box">
+                <div class= "sobre">
+                <div class= "pais-nome">${pais.name.common}</div>
+                <div class="pais-capital">${pais.capital}</div>
+                </div>
+                <img src="${pais.flags.svg}" alt="Bandeira de ${pais.name.common}" class="pais-bandeira">
+            </div>
+        `;
+    });
+}
